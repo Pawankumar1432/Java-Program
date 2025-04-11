ee93ce073e0a171ca801e2a9bd5dde8dab052fe8
Pavankumar <byallpawan@gmail.com>
2025-04-11 23:28:18 +0530

    first : commit

--- problemsolving/src/problemsolving/LeapYearCheck.java
+++ problemsolving/src/problemsolving/LeapYearCheck.java
@@ -7,7 +7,7 @@ public class LeapYearCheck {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		System.out.print("Enter a year: ");
+		System.out.print("Enter a year  ");
 		int year = sc.nextInt();
 
 		if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {

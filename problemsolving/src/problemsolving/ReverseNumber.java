2cc1e27af32164144616a8c716902d6258a97c37
Pavankumar <byallpawan@gmail.com>
2025-04-11 23:28:45 +0530

    first : commit

--- problemsolving/src/problemsolving/ReverseNumber.java
+++ problemsolving/src/problemsolving/ReverseNumber.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class ReverseNumber {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the number : ");
+		System.out.println("Enter the number :");
 		int n=sc.nextInt();
 		int rev=0;
 		while(n>0) {

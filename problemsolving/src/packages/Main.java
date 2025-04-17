fe6931019f8330459455edf930e4c7207d0bfcd5
Pavankumar <byallpawan@gmail.com>
2025-04-17 23:33:16 +0530

    first : commit

--- problemsolving/src/packages/Main.java
+++ problemsolving/src/packages/Main.java
@@ -4,6 +4,5 @@ public class Main {
 	public static void main(String[] args) {
 		java.lang.System.out.println("h pad=f");
 		java.lang.System.out.println("Hi there!");
-		java.lang.System.out.println("This is a test of the Java package system.");
-	}
+ 	}
 }

421ec43aa8ea4d0077601c51fdf4983772436627
Pavankumar <byallpawan@gmail.com>
2025-04-21 22:25:53 +0530

    first : commit

--- problemsolving/src/problemsolving/FactorsOfGivenNumber.java
+++ problemsolving/src/problemsolving/FactorsOfGivenNumber.java
@@ -10,7 +10,7 @@ public class FactorsOfGivenNumber {
 		if (n > 0) {
 			for (int i = 1; i <= n; i++) {
 				if (n % i == 0) {
-					System.out.print(i + " ");
+					System.out.print(i +" ");
 				}
 			}
 		} else {

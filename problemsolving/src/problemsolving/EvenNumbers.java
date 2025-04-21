9e42d518152ffa18075306e7c256b77e5fc109d8
Pavankumar <byallpawan@gmail.com>
2025-04-21 23:18:44 +0530

    first : commit

--- problemsolving/src/problemsolving/EvenNumbers.java
+++ problemsolving/src/problemsolving/EvenNumbers.java
@@ -4,7 +4,7 @@ public class EvenNumbers {
 	public static void main(String[] args) {
 		// Print even numbers from 1 to 50
 		for (int i = 2; i <= 50; i += 2) {
-			System.out.println(i);
+			System.out.println(i+" ");
 		}
 		
 		

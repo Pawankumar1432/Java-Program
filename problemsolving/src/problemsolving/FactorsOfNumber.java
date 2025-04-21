86a8f4a03791eb8960eccb22ad66128306e7a0fc
Pavankumar <byallpawan@gmail.com>
2025-04-21 22:25:39 +0530

    first : commit

--- problemsolving/src/problemsolving/FactorsOfNumber.java
+++ problemsolving/src/problemsolving/FactorsOfNumber.java
@@ -5,10 +5,10 @@ public class FactorsOfNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter a number: ");
+        System.out.print("Enter a number : ");
         int number = sc.nextInt();
 
-        System.out.println("Factors of " + number + " are:");
+        System.out.println("Factors of " + number + " are :");
         for (int i = 1; i <= number; i++) {
             if (number % i == 0) {
                 System.out.println(i);

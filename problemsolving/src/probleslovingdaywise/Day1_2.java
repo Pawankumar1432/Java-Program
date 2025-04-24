65265a4eda75d233c5ec45f4ca66c6bcddd2410b
Pavankumar <byallpawan@gmail.com>
2025-04-24 23:30:02 +0530

    Create Day1_2.java

---
+++ problemsolving/src/probleslovingdaywise/Day1_2.java
@@ -0,0 +1,28 @@
+package probleslovingdaywise;
+
+public class Day1_2 {
+public static void main(String[] args) {
+	
+	
+	/*
+	 * Question 2: Fibonacci Series
+	 * Problem Statement:
+	 * Write a Java program to print the Fibonacci series up to the first 10 numbers.
+	 * The Fibonacci sequence starts from 0 and 1, and each number is the sum of the previous two.
+	 * Sample Output:
+	 * Fibonacci series up to 10 numbers:  
+	 * 0 1 1 2 3 5 8 13 21 34
+	 * */
+int a=0;
+int b=1;
+int c=0;
+System.out.println("Fibonacci series up to 10 numbers : ");
+System.out.print(a+" ");
+for(int i=2;i<=10;i++) {
+	a=b;
+	b=c;
+	c=a+b;
+	System.out.print(c+" ");
+}
+}
+}

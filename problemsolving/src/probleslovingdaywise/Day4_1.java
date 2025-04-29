f625de7f90a1c995b4deeb0bbda65563f526d515
Pavankumar <byallpawan@gmail.com>
2025-04-30 00:04:30 +0530

    Update Day4_1.java

--- problemsolving/src/probleslovingdaywise/Day4_1.java
+++ problemsolving/src/probleslovingdaywise/Day4_1.java
@@ -4,27 +4,37 @@ import java.util.Scanner;
 
 public class Day4_1 {
 	public static void main(String[] args) {
-		/*Question 1: Print Non-Fibonacci Numbers
-		 * Problem Statement: 
-		 * Write a Java program to print the first n non-Fibonacci numbers.
-		 * A non-Fibonacci number is a positive integer that is not a Fibonacci number.
-		 * Sample Input:  
-         Enter the count: 20
-         Sample Output: 
-         4, 6, 7, 9, 10, 11, 12, 14, 15, 16, 17, 18, 19, 20
-
-		 * */
-		Scanner sc=new Scanner(System.in);
+		/*
+		 * Question 1: Print Non-Fibonacci Numbers Problem Statement: Write a Java
+		 * program to print the first n non-Fibonacci numbers. A non-Fibonacci number is
+		 * a positive integer that is not a Fibonacci number. Sample Input: Enter the
+		 * count: 20 Sample Output: 4, 6, 7, 9, 10, 11, 12, 14, 15, 16, 17, 18, 19, 20
+		 * 
+		 */
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a count : ");
-		int n=sc.nextInt();
-		int a=0;
-		int b=1;
-		int c=0;
-		for(int i=3;i<n;i++) {
-			c=a+b;
-			
+		int n = sc.nextInt();
+		int a = 1;
+		int b = 2;
+
+		int count = 0;
+		while (count < n) {
+			int c = a + b;
+			int c1 = 1;
+			for (int i = b + 1; i < c; i++) {
+				if (i > 20) {
+					c1 = 0;
+					break;
+				}
+				System.out.print(i + " ");
+				count++;
+			}
+			a = b;
+			b = c;
+			if (c1 == 0)
+				break;
 		}
-		
+
 	}
 
 }

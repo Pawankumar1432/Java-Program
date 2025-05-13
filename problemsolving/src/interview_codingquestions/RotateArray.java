cb8729e8dcc863a1f886f68b918e296d48dd1292
Pavankumar <byallpawan@gmail.com>
2025-05-13 21:55:12 +0530

    Create RotateArray.java

---
+++ problemsolving/src/interview_codingquestions/RotateArray.java
@@ -0,0 +1,36 @@
+package interview_codingquestions;
+
+import java.util.Scanner;
+
+public class RotateArray {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+	int []a= {1,2,3,4,5,6};
+	int numberRotate=2;
+	//Naive Approach  Time complexity (n*d)
+//	for(int i=0;i<numberRotate;i++) {
+//		int first=a[0];
+//		for(int j=0;j<a.length;i++) {
+//			a[j]=a[j+1];
+//		}
+//		a[a.length-1]=first;
+//	}
+//	
+	//Better Way Approach
+	int []temp=new int[a.length];
+	numberRotate%=a.length;
+	for(int i=0;i<a.length-numberRotate;i++) {
+		temp[i]=a[numberRotate+i];
+	}
+	for(int i=0;i<numberRotate;i++) {
+		temp[a.length-numberRotate+i]=a[i];
+	}
+	
+	for(int i=0;i<a.length;i++) {
+		a[i]=temp[i];
+		System.out.print(a[i]+" ");
+	}
+	
+	}
+
+}

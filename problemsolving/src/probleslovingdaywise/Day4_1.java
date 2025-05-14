b9304cf9fa2caa57bb1fd4394efaff07be8e59ba
Pavankumar <byallpawan@gmail.com>
2025-05-15 00:10:15 +0530

    Update Day4_1.java

--- problemsolving/src/probleslovingdaywise/Day4_1.java
+++ problemsolving/src/probleslovingdaywise/Day4_1.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Day4_1 {
 	public static void main(String[] args) {
 		/*
-		 * Question 1: Print Non-Fibonacci Numbers Problem Statement: Write a Java
+		 * Question 1: 
+		 * sPrint Non-Fibonacci Numbers Problem Statement: Write a Java
 		 * program to print the first n non-Fibonacci numbers. A non-Fibonacci number is
 		 * a positive integer that is not a Fibonacci number. Sample Input: Enter the
 		 * count: 20 Sample Output: 4, 6, 7, 9, 10, 11, 12, 14, 15, 16, 17, 18, 19, 20

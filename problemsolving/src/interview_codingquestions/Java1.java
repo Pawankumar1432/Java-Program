6003eb81003bd1a04c86286572b4952476b860a6
Pavankumar <byallpawan@gmail.com>
2025-05-17 22:42:42 +0530

    Create Java1.java

---
+++ problemsolving/src/interview_codingquestions/Java1.java
@@ -0,0 +1,17 @@
+package interview_codingquestions;
+
+
+
+interface Hello
+{
+	
+}
+
+public class Java1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

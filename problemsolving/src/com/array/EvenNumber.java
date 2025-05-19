6e459bd0e1ef96d9acd818ebb720542dee9fdfe8
Pavankumar <byallpawan@gmail.com>
2025-05-19 20:59:30 +0530

    Update EvenNumber.java

--- problemsolving/src/com/array/EvenNumber.java
+++ problemsolving/src/com/array/EvenNumber.java
@@ -4,29 +4,41 @@ public class EvenNumber {
 	public static void main(String[] args) {
 		int[]a= {10,2,3,4,5,6,22};
 		 
+		
+		
+		System.out.println(a.length);
+		System.out.println(EvenNumber.class);
+		
+		
+		
+		
 		int even_Sum=0,odd_Sum=0;
-		for(int i=0;i<a.length;i++) {
-			if(a[i]%2==0) {
-				System.out.println(a[i]);
-				even_Sum+=a[i];
-			}else {
-				odd_Sum+=a[i];
-			}
-		}
-		System.out.println("Even Sum : "+even_Sum+"\nOdd_Sum: "+odd_Sum);
 		
+		//System.out.println("Array elements : ");
+//	   int[]b=new int[10];
 		
-		
-		int odd_Index=0;
-		for(int i=0;i<a.length;i++) {
-		 if(i%2!=0) {
-			 odd_Index+=a[i];
-			 System.out.print(i+" index : "+a[i]+" ");
-		 }
-		}
-		System.out.println("Sum of Odd index values sum : "+odd_Index);
-		
-		
+//		
+//		System.out.print("Even numbers : ");
+//		for(int i=0;i<a.length;i++) {
+//			if(a[i]%2==0) {
+//				System.out.print(a[i]+" ");
+//				even_Sum+=a[i];
+//			}else {
+//				odd_Sum+=a[i];
+//			}
+//		}
+//		System.out.println("\nEven Sum : "+even_Sum+"\nOdd_Sum: "+odd_Sum);
+//		
+//		int odd_Index=0;
+//		for(int i=0;i<a.length;i++) {
+//		 if(i%2!=0) {
+//			 odd_Index+=a[i];
+////			 System.out.print(i+" index : "+a[i]+" ");
+//		 }
+//		}
+//		System.out.println("\nSum of Odd index values sum : "+odd_Index);
+//		
+//		
 	}
 
 }

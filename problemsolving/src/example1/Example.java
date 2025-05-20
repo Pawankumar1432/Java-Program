793f2eee6fab25fcf386f82dc6a0addee96b9618
Pavankumar <byallpawan@gmail.com>
2025-05-20 23:36:01 +0530

    Update Example.java

--- problemsolving/src/example1/Example.java
+++ problemsolving/src/example1/Example.java
@@ -1,19 +1,38 @@
 package example1;
+ 
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.event.*;import javax.swing.*;
+import java.awt.Color;
+import java.awt.event.*;
 
- abstract class Ex {
- abstract void display() ;
-}
-public class Example extends Ex{
-public static void main(String[] args) {
-	Example a=new Example();
-		a.display();
-	}
-	@Override
-	void display() {
-		// TODO Auto-generated method stub
-		System.out.println("display");
-		
-	}
-	
-}
+class Example extends JFrame {
 
+  public Example(){
+    String text = new String("I'm a simple Program ");
+    String text2 = text.concat(
+      "that uses classes and objects");
+
+    JLabel label = new JLabel(text2);
+    JPanel panel = new JPanel();
+    panel.setBackground(Color.white);
+
+    getContentPane().add(panel);
+    panel.add(label);
+  }
+
+  public static void main(String[] args){
+    Example frame = new Example();
+
+    frame.setTitle("Fruit $1.25 Each");
+    WindowListener l = new WindowAdapter() {
+      public void windowClosing(WindowEvent e) {
+        System.exit(0);
+      }
+    };
+
+    frame.addWindowListener(l);
+    frame.pack();
+    frame.setVisible(true);
+  }
+}

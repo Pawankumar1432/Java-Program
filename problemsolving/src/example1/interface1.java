1bed8763f607a533f30100eca8a640c1327704bb
Pavankumar <byallpawan@gmail.com>
2025-05-21 16:31:03 +0530

    Create interface1.java

---
+++ problemsolving/src/example1/interface1.java
@@ -0,0 +1,15 @@
+package example1;
+
+public class interface1 {
+	int a;
+	 
+	 public static void main(String[] args) {
+	 final interface1 a= new interface1();
+	 a.a=10;
+	 
+	 System.out.println(a.a);
+	
+		
+	}
+}
+

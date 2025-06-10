cca5ee80ae66544892d31a507083f6620c7fa31e
Pavankumar <byallpawan@gmail.com>
2025-06-10 18:09:54 +0530

    Create Sub.java

---
+++ problemsolving/src/com/array/matrix/Sub.java
@@ -0,0 +1,23 @@
+package com.array.matrix;
+
+public class Sub {
+	public static void main(String[] args) {
+		int[][] a = { { 1, 2, 5 }, { 4, 5, 6 }, { 7, 8, 9 } };
+		int[][] b = { { 1, 2, 3 }, { 4, 5, 6 }, { 1, 2, 3 } };
+		int[][] result=new int[a.length][a[0].length];
+		for (int i = 0; i < a.length; i++) {
+			for(int j=0;j<a[0].length;j++) {
+				result[i][j]=a[i][j]-b[i][j];
+			}
+		}
+			for (int i = 0; i < result.length; i++) {
+				for (int j = 0; j < result[0].length; j++) {
+					System.out.print(result[i][j] + " ");
+				}
+				System.out.println();
+			}
+
+	}
+
+}
+
